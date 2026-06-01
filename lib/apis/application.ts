3149779e793db0194f1aa5a8f7813386ce7a5507
Nguyen Nhut Tan <nguyentan@MacBook-Pro-cua-Nguyen.local>
2026-06-01 17:16:18 +0700

    feat(app-api): add listApplications, getApplicationDetail, updateApplication

--- lib/apis/application.ts
+++ lib/apis/application.ts
@@ -1,8 +1,27 @@
 import http from "@/lib/http";
-import {ApplicationRequest} from "@/lib/apis/application-types";
+import { ApplicationRequest, ApplicationUpdateResponse, UpdateApplicationRequest } from "@/lib/apis/application-types";
+import { ApiResponse, PaginatedResponse, UnderwritingDetailResponse, UnderwritingListItem, UnderwritingQueueParams } from "@/lib/apis/underwriting-types";
 
 const Application = {
-  createApplication: (payload: ApplicationRequest) => http.post("/loan-application/create", payload)
-}
+  createApplication: (payload: ApplicationRequest) =>
+    http.post("/loan-application/create", payload),
+
+  listApplications: (params?: UnderwritingQueueParams) =>
+    http.get<ApiResponse<PaginatedResponse<UnderwritingListItem>>>(
+      "/loan-application/underwriting/queue",
+      { params }
+    ),
+
+  getApplicationDetail: (id: number) =>
+    http.get<ApiResponse<UnderwritingDetailResponse>>(
+      `/loan-application/${id}/underwriting-detail`
+    ),
+
+  updateApplication: (id: number, payload: UpdateApplicationRequest) =>
+    http.post<ApiResponse<ApplicationUpdateResponse>>(
+      `/loan-application/${id}/update`,
+      payload
+    ),
+};
 
 export default Application;
